d7b14458db3625c3baba5b16a49f199ca49b259a
Wael-Morjen <waelmorjen@gmail.com>
2024-03-07 18:58:09 +0100

    added boardlist component and Info

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,10 +1,16 @@
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/Info";
+import { BoardList } from "./_components/BoardList";
+
 const OrganizationIdPage = () => {
 
   return (
-    <div>
-      <form action="">
-        
-      </form>
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4"/>
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 }
