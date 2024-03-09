c7a3d4cd241e4caa7c0ceccca9350c12fdc79b0a
Wael-Morjen <waelmorjen@gmail.com>
2024-03-09 18:35:59 +0100

    get the board and the orgId

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,38 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+
+const BoardIdPageLayout = async ({ 
+  children, 
+  params
+}: { 
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org")
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
+  return (
+    <div>
+      <main className="relative pt-28 h-full">
+        {children}
+      </main>
+    </div>
+  );
+}
+ 
+export default BoardIdPageLayout;
