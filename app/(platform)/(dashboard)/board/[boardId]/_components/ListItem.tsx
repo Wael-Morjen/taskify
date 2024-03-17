227a3f5fd78316f07462c8a12e9cb15040207afb
Wael-Morjen <waelmorjen@gmail.com>
2024-03-17 23:19:10 +0100

    add card form

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
@@ -3,6 +3,8 @@
 
 import { ListWithCards } from "@/types";
 import { ListHeader } from "./ListHeader";
+import { ElementRef, useRef, useState } from "react";
+import { CardForm } from "./CardForm";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -13,11 +15,33 @@ export const ListItem = ({
   data,
   index
 }: ListItemProps) => {
+  const textAreaRef = useRef<ElementRef<"textarea">>(null)
+  const [isEditing, setIsEditing] = useState(false);
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const enableEditing = () => {
+    setIsEditing(true);
+    setTimeout(() => {
+      textAreaRef.current?.focus();
+    })
+  }
+
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
         <ListHeader 
+          onAddCard={enableEditing}
           data={data}
+        />
+        <CardForm 
+          listId={data.id}
+          ref={textAreaRef}
+          isEditing={isEditing}
+          enableEditing={enableEditing}
+          disableEditing={disableEditing}
         />
       </div>
     </li>
