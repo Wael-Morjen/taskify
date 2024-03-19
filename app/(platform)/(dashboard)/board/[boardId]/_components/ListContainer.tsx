b9d96d980f2891a80bbe7ffe83fa3774e559231b
Wael-Morjen <waelmorjen@gmail.com>
2024-03-19 23:18:12 +0100

    onDragEnd server actions

--- app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/ListContainer.tsx
@@ -9,6 +9,8 @@ import { DragDropContext, Droppable } from "@hello-pangea/dnd"
 
 import { useAction } from "@/hooks/use-action";
 import { updateListOrder } from "@/actions/update-list-order";
+import { updateCardOrder } from "@/actions/update-card-order";
+
 import { toast } from "sonner";
 
 interface ListContainerProps {
@@ -37,7 +39,16 @@ export const ListContainer = ({
     onError: (error) => {
       toast.error(error);
     }
-  })
+  });
+
+  const { execute: executeUpdateCardOrder } = useAction(updateCardOrder, {
+    onSuccess: () => {
+      toast.success("Card reordered");
+    },
+    onError: (error) => {
+      toast.error(error);
+    }
+  });
 
   useEffect(() => {
     setOrderedData(data);
@@ -104,8 +115,10 @@ export const ListContainer = ({
         sourceList.cards = reorderedCards;
 
         setOrderedData(newOrderedData);
-
-        // TODO: trigger server action
+        executeUpdateCardOrder({
+          boardId: boardId,
+          items: reorderedCards
+        })
 
         // User moves a card to another list
       } else {
@@ -128,7 +141,10 @@ export const ListContainer = ({
         });
 
         setOrderedData(newOrderedData);
-        // Trigger server action
+        executeUpdateCardOrder({
+          boardId: boardId,
+          items: destList.cards
+        })
       }
     }
   }
