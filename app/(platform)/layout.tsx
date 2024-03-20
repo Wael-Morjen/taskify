d4deec8808d1ae4654a53322c8df1541d9a48e20
Wael-Morjen <waelmorjen@gmail.com>
2024-03-20 23:15:24 +0100

    add the queryprovider to platform layout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,13 +1,17 @@
+import { ModalProvider } from "@/components/providers/ModalProvider";
+import { QueryProvider } from "@/components/providers/QueryProvider";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 }
- 
 export default PlatformLayout;
